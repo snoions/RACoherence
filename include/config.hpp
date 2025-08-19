4adf0d9f1a8e3d3aea556c9857806fe39ca2298f
Simon Guo <simon@plrg2.ics.uci.edu>
2025-08-18 20:34:08 -0700

    inline handle_load and handle_store

--- include/config.hpp
+++ include/config.hpp
@@ -40,6 +40,6 @@ constexpr unsigned TOTAL_OPS = 10000 * (1ull << 6);
 //#define LOCAL_CL_TABLE_BUFFER
 
 // use sequential workload
-//#define SEQ_WORKLOAD
+#define SEQ_WORKLOAD
 
 #endif
--- include/user.hpp
+++ include/user.hpp
@@ -40,9 +40,71 @@ class User {
 public:
     User(CXLPool &pool, NodeLocalMeta &local_meta): cxl_meta(pool.meta), cxl_data(pool.data), cache_info(local_meta.cache_info) {}
 
-    void handle_store(char *addr, bool is_release = false);
+    inline char handle_load(char *addr, bool is_acquire = false) {
+            if (cache_info.invalidate_if_dirty(addr)) {
+#ifdef STATS
+                invalidate_count++;
+#endif
+            }
 
-    char handle_load(char *addr, bool is_acquire = false);
+        char ret;
+        if (is_acquire) {
+            size_t off = addr - cxl_data;
+            auto at_clk = cxl_meta.atmap[off].get([&](auto &self) {
+                ret = ((volatile std::atomic<char> *)addr)->load(std::memory_order_acquire);
+                return self.clock;
+            });
+
+            LOG_INFO("node " << node_id << " acquire " << (void*) addr << std::dec << ", clock=" << at_clk)
+
+#ifdef USER_HELP_CONSUME
+            user_help_consume(at_clk);
+#else
+            wait_for_consume(at_clk);
+#endif
+
+        } else
+            ret = *((volatile char *)addr);
+        return ret;
+    }
+
+
+    inline void handle_store(char *addr, bool is_release = false) {
+        uintptr_t cl_addr = (uintptr_t)addr & CACHE_LINE_MASK;
+
+        while (dirty_cls.insert(cl_addr) || dirty_cls.get_length_entry_count() != 0)
+            write_to_log(false);
+
+        if(is_release) {
+#ifdef LOCAL_CL_TABLE_BUFFER
+            while (dirty_cls.dump_buffer_to_table())
+                write_to_log(false);
+#endif
+
+            clock_t clk_val = write_to_log(true);
+            thread_clock.merge(node_id, clk_val);
+            LOG_INFO("node " << node_id << " release at " << (void *)addr << std::dec << ", thread clock=" <<thread_clock)
+            size_t off = addr - cxl_data;
+#ifdef LOCATION_CLOCK_MERGE
+            cxl_meta.atmap[off].mod([&](auto &self) {
+                self.clock.merge(thread_clock);
+            });
+            ((volatile std::atomic<char> *)addr)->store(0, std::memory_order_release);
+#else
+            cxl_meta.atmap[off].mod([&](auto &self) {
+                self.clock = thread_clock;
+                ((volatile std::atomic<char> *)addr)->store(0, std::memory_order_release);
+            });
+#endif
+        } else {
+            if (cache_info.invalidate_if_dirty(addr)) {
+#ifdef STATS
+                invalidate_count++;
+#endif
+            }
+            *((volatile char *)addr) = 0;
+        }
+    }
 
     inline void handle_store_raw(char *addr, bool is_release = false) {
         //assuming we don't know which writes overwrite entire cache lines
